a033ce2829590cee370a46fe2a470142cf0d5118
Arles Torquato de Farias <farias.arles@gmail.com>
2018-01-28 10:23:14 -0300

    Add code to check if date fields are filled correctly

--- src/script.js
+++ src/script.js
@@ -2,6 +2,13 @@ function calcula(){
     var valorPlano = document.querySelector('input[name=valor]:checked').value;
     var dataInicio = new Date(document.querySelector('input[name=dataInicio]').value);
     var dataFim = new Date(document.querySelector('input[name=dataFim]').value);
+    
+    //checar se os campos de datas foram preenchidos corretamente
+    if((!Date.parse(dataInicio)) || (!Date.parse(dataFim))){
+        alert("Preencha corretamente os campos de data");
+        return;
+    }
+
     //864000000 ms em um dia
     var diasUtilizados = (dataFim - dataInicio)/86400000;
     var resultado = ((diasUtilizados/30)*valorPlano).toFixed(2);
