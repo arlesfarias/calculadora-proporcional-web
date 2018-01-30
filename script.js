832d48a4bc2262eb0dc0627fd973538aecffd5df
Arles Torquato de Farias <farias.arles@gmail.com>
2018-01-29 22:50:17 -0300

    Format document with prettier

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
-function calcula(){
-    var alertaDiv = document.getElementById("alerta");
-    var valorPlano = document.querySelector('input[name=valor]:checked').value;
-    var dataInicio = new Date(document.querySelector('input[name=dataInicio]').value);
-    var dataFim = new Date(document.querySelector('input[name=dataFim]').value);
-    
-    //checar se os campos de datas foram preenchidos corretamente
-    if((!Date.parse(dataInicio)) || (!Date.parse(dataFim))){
-        alertaDiv.innerHTML = "Preencha corretamente os campos de data!";
-        alertaDiv.removeAttribute('hidden');
-        return;
-    }
+function calcula() {
+  var alertaDiv = document.getElementById("alerta");
+  var valorPlano = document.querySelector("input[name=valor]:checked").value;
+  var dataInicio = new Date(
+    document.querySelector("input[name=dataInicio]").value
+  );
+  var dataFim = new Date(document.querySelector("input[name=dataFim]").value);
 
-    //evita que o usuário coloque uma data fim menor que a data inicio
-    if(dataFim < dataInicio){
-        alertaDiv.innerHTML = "A data final deve ser maior que a inicial!";
-        alertaDiv.removeAttribute('hidden');
-        return;
-    }
+  //checar se os campos de datas foram preenchidos corretamente
+  if (!Date.parse(dataInicio) || !Date.parse(dataFim)) {
+    alertaDiv.innerHTML = "Preencha corretamente os campos de data!";
+    alertaDiv.removeAttribute("hidden");
+    return;
+  }
 
-    //864000000 ms em um dia
-    var diasUtilizados = (dataFim - dataInicio)/86400000;
-    var valor = ((diasUtilizados/30)*valorPlano).toFixed(2);
+  //evita que o usuário coloque uma data fim menor que a data inicio
+  if (dataFim < dataInicio) {
+    alertaDiv.innerHTML = "A data final deve ser maior que a inicial!";
+    alertaDiv.removeAttribute("hidden");
+    return;
+  }
 
-    //formata a frase que será mostrada para o usuário
-    var resultado = "O valor a ser pago para utilizar por " + diasUtilizados + " dias o plano de R$ " + parseFloat(valorPlano).toFixed(2) + " é: <b>R$ " + valor + "</b>";
+  //864000000 ms em um dia
+  var diasUtilizados = (dataFim - dataInicio) / 86400000;
+  var valor = (diasUtilizados / 30 * valorPlano).toFixed(2);
 
-    document.getElementById("resultado").innerHTML = resultado;
-    document.getElementById("resultado").removeAttribute('hidden');
-    alertaDiv.setAttribute("hidden", null);
+  //formata a frase que será mostrada para o usuário
+  var resultado =
+    "O valor a ser pago para utilizar por " +
+    diasUtilizados +
+    " dias o plano de R$ " +
+    parseFloat(valorPlano).toFixed(2) +
+    " é: <b>R$ " +
+    valor +
+    "</b>";
+
+  document.getElementById("resultado").innerHTML = resultado;
+  document.getElementById("resultado").removeAttribute("hidden");
+  alertaDiv.setAttribute("hidden", null);
 }
 
-function limpa(){
-    document.querySelector('input[name=dataInicio]').value = null;
-    document.querySelector('input[name=dataFim]').value = null;
-    document.getElementById("resultado").setAttribute("hidden", null);
+function limpa() {
+  document.querySelector("input[name=dataInicio]").value = null;
+  document.querySelector("input[name=dataFim]").value = null;
+  document.getElementById("resultado").setAttribute("hidden", null);
 }
